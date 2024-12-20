7193c020e2486f3c9b9d78f89315de35d45dd876
tim93009 <tim93009@gmail.com>
2024-12-20 16:29:21 +0800

    2024/12/20 p.m.04:29 頁碼4-37

---
+++ TeaTimeDemo/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TeaTimeDemo.Data;
+using TeaTimeDemo.Models;
+
+namespace TeaTimeDemo.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}

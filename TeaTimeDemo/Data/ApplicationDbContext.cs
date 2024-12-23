095a4efb9574a87ce39dc7ca94c9b2996cdff993
tim93009 <tim93009@gmail.com>
2024-12-23 16:26:15 +0800

    2024/12/23 p.m.04:25 書本4-38

--- TeaTimeDemo/Data/ApplicationDbContext.cs
+++ TeaTimeDemo/Data/ApplicationDbContext.cs
@@ -7,7 +7,6 @@ namespace TeaTimeDemo.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-
         }
 
         public DbSet<Category> Categories { get; set; }

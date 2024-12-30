aeae01cbc489191f8ac6e0272b4b6c0999a98649
tim93009 <tim93009@gmail.com>
2024-12-30 13:25:37 +0800

    同步專案

--- TeaTimeDemo.DataAccess/Repository/IRepository/ICategoryRepository.cs
+++ TeaTimeDemo.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -10,6 +10,6 @@ namespace TeaTimeDemo.DataAccess.Repository.IRepository
     public interface ICategoryRepository : IRepository<Category>
     {
         void Update(Category obj);
-        void Save();
+        //void Save();
     }
 }

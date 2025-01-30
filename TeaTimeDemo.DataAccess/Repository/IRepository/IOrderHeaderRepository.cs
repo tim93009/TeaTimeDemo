41a80f339edcd1ccc0e50af83096e440699d498f
LAPTOP-7GET9CKD\USER <tim93009@gmail.com>
2025-01-31 02:47:26 +0800

    book 10-end

--- TeaTimeDemo.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
+++ TeaTimeDemo.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
@@ -10,6 +10,6 @@ namespace TeaTimeDemo.DataAccess.Repository.IRepository
     public interface IOrderHeaderRepository : IRepository<OrderHeader>
     {
         void Update(OrderHeader obj);
-        
+        void UpdateStatus(int id, string orderStatus, string? paymentStatus = null);
     }
 }
--- TeaTimeDemo/Areas/Customer/Controllers/CartController.cs
+++ TeaTimeDemo/Areas/Customer/Controllers/CartController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TeaTimeDemo.DataAccess.Repository.IRepository;
+using TeaTimeDemo.Models;
 using TeaTimeDemo.Models.ViewModels;
+using TeaTimeDemo.Utility;
 
 namespace TeaTimeDemo.Areas.Customer.Controllers
 {
@@ -11,6 +13,7 @@ namespace TeaTimeDemo.Areas.Customer.Controllers
     public class CartController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        [BindProperty]
         public ShoppingCartVM ShoppingCartVM { get; set; }
 
         public CartController(IUnitOfWork unitOfWork)
@@ -59,7 +62,67 @@ namespace TeaTimeDemo.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
-        public IActionResult Plus(int cartId)
+		[HttpPost]
+		[ActionName("Summary")]
+		public IActionResult SummaryPOST(ShoppingCartVM shoppingCartVM)
+		{
+			var claimsIdentity = (ClaimsIdentity)User.Identity;
+			var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+
+			ShoppingCartVM.ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product");
+
+            //訂單日期為今天日期
+			ShoppingCartVM.OrderHeader.OrderDate = System.DateTime.Now;
+
+			ShoppingCartVM.OrderHeader.ApplicationUserId = userId;
+
+			ShoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId);
+
+            //計算訂單總金額
+			foreach (var cart in ShoppingCartVM.ShoppingCartList)
+			{
+				ShoppingCartVM.OrderHeader.OrderTotal += (cart.Product.Price * cart.Count);
+			}
+
+			_unitOfWork.OrderHeader.Add(ShoppingCartVM.OrderHeader);
+			_unitOfWork.Save();
+
+            //創建訂單細節資訊
+			foreach (var cart in ShoppingCartVM.ShoppingCartList)
+			{
+				OrderDetail orderDetail = new()
+				{
+					ProductId = cart.ProductId,
+					OrderHeaderId = ShoppingCartVM.OrderHeader.Id,
+					Ice = cart.Ice,
+					sweetness = cart.Sweetness,
+					Price = cart.Product.Price,
+					Count = cart.Count
+
+				};
+				_unitOfWork.OrderDetail.Add(orderDetail);
+				_unitOfWork.Save();
+			}
+
+            //在這邊重導向到一個動作，並將訂單編號傳遞下去
+			return RedirectToAction(nameof(OrderConfirmation), new { id = ShoppingCartVM.OrderHeader.Id });
+		}
+
+        //訂單確認頁面
+		public IActionResult OrderConfirmation(int id)
+		{
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == id, includeProperties: "ApplicationUser"); //送出後等待店家確認訂單
+            _unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusPending); //送出訂單後將購物車內的商品刪除
+
+            List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
+            _unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
+            _unitOfWork.Save();
+
+            return View(id);
+        }
+
+		public IActionResult Plus(int cartId)
         {
             var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
             cartFromDb.Count += 1;
